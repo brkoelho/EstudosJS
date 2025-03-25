013ef8f1384191ab3b6eea1a230a9c9207c51d98
Bruno Rodrigues Coelho <brkoelho@hotmail.com>
2025-03-25 09:00:36 -0300

    Delete estruturas_de_controle directory

--- estruturas_de_controle/for_1.js
+++
@@ -1,3 +0,0 @@
-for (let controle = 1; controle <= 10; controle++) {
-    console.log(controle);
-}
--- estruturas_de_controle/for_3.js
+++
@@ -1,6 +0,0 @@
-// Fazer FOR que gere do 7 até o 3.
-
-for (let x = 7; x >= 3; x--) {
-    console.log(x);
-}
-console.log("Fim!");
